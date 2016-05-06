3d389761691934dc70ee761964848a200d3535cd
Niraj <niraj.mulji@gmail.com>
2016-05-06 10:01:19 -0500

    This is my data file for my table exercise.

---
+++ albums_seeder.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', '1982', '48100000', 'Pop, Rock, R&B'),
+       ('Eagles', 'Their Greatest Hits', '1976', '32200000', 'Rock, Soft Rock, Folk Rock'),
+       ('AC/DC', 'Back in Black', '1980', '25900000', 'Hard Rock'),
+       ('Pink Floyd', 'The Dark Side of the Moon', '1973', '22700000', 'Progressive Rock'),
+       ('Whitney Houston', 'The Bodyguard', '1992', '27400000', 'Soundtrack/R&B, Soul, Pop'),
+       ('Meat Loaf', 'Bat Out of Hell', '1977', '20600000', 'Hard Rock, Progressive Rock'),
+       ('Bee Gees', 'Saturday Night Fever', '1977', '19000000', 'Disco'),
+       ('Fleetwood Mac', 'Rumours', '1977', '27900000', 'Soft Rock'),
+       ('Led Zeppelin', 'Led Zeppelin IV', '1971', '29000000', 'Hard Rock, Heavy Metal'),
+       ('Shania Twain', 'Come On Over', '1997', '29600000', 'Country, Pop');
