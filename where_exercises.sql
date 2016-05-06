47e19bd047e52d63cf09651b0c3a8be6edbc4953
Niraj <niraj.mulji@gmail.com>
2016-05-06 15:54:45 -0500

    Employees with a 'q' in their last name — 1,873 rows.

--- where_exercises.sql
+++ where_exercises.sql
@@ -13,3 +13,7 @@ WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 SELECT birth_date
 FROM employees
 WHERE birth_date LIKE '%-12-25';
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%';
