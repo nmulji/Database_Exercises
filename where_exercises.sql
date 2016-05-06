895564a966e38ee7914798ced99bb382132a11ff
Niraj <niraj.mulji@gmail.com>
2016-05-06 15:53:44 -0500

    Employees born on Christmas — 842 rows.

--- where_exercises.sql
+++ where_exercises.sql
@@ -8,4 +8,8 @@ WHERE last_name LIKE 'E%';
 
 SELECT hire_date
 FROM employees
-WHERE hire_date BETWEEn '1990-01-01' AND '1999-12-31';
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
+SELECT birth_date
+FROM employees
+WHERE birth_date LIKE '%-12-25';
