822cba55c7ce4a8e3379975006a28de83a440a88
Niraj <niraj.mulji@gmail.com>
2016-05-06 15:50:39 -0500

    This is my commit after doing the following step -  Employees hired in the 90s — 135,214 rows.

--- where_exercises.sql
+++ where_exercises.sql
@@ -5,3 +5,7 @@ WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 SELECT last_name
 FROM employees
 WHERE last_name LIKE 'E%';
+
+SELECT hire_date
+FROM employees
+WHERE hire_date BETWEEn '1990-01-01' AND '1999-12-31';
