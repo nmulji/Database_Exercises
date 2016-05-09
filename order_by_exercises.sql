1bee87c4d89134ecf29532c5f10ef5d266f3e82c
Niraj <niraj.mulji@gmail.com>
2016-05-09 10:39:30 -0500

    This is my exercise after step 7 - Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last. It should be Khun Bernini.

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -17,12 +17,14 @@ WHERE last_name LIKE 'E%'
 )
 ORDER BY emp_no DESC;
 
-SELECT hire_date
+SELECT first_name, last_name, hire_date, birth_date
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
 	AND (
 	birth_date LIKE '%-12-25'
-);
+)
+ORDER BY birth_date ASC, hire_date DESC;
+
 
 SELECT birth_date
 FROM employees
