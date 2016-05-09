81cfb96397e09464f8f8b301556ce04c26a48b4c
Niraj <niraj.mulji@gmail.com>
2016-05-09 13:31:53 -0500

    This is my commit after counting the genders.

---
+++ functions_exercises.sql
@@ -0,0 +1,36 @@
+SELECT COUNT(gender), gender
+FROM employees
+WHERE first_name = 'Irena'
+		OR first_name = 'Vidya'
+		OR first_name = 'Maya'
+GROUP BY gender ASC;
+
+SELECT emp_no, last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+	AND (
+	last_name LIKE '%e'
+)
+ORDER BY emp_no DESC;
+
+SELECT first_name, last_name, hire_date, birth_date
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+	AND (
+	birth_date LIKE '%-12-25'
+)
+ORDER BY birth_date ASC, hire_date DESC;
+
+
+SELECT birth_date
+FROM employees
+WHERE birth_date LIKE '%-12-25';
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+	AND (
+	last_name NOT LIKE '%qu%'
+);
+
+SELECT * FROM employees
