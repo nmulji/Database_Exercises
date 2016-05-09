2f7c889d284d7d30f7ff17d89ea721b33403e494
Niraj <niraj.mulji@gmail.com>
2016-05-09 09:43:43 -0500

    This is my exercise after step 6 - Find all employees hired in the 90s and born on Christmas — 362 rows.

--- where_exercises.sql
+++ where_exercises.sql
@@ -12,11 +12,14 @@ FROM employees
 WHERE last_name LIKE 'E%'
 	AND (
 	last_name LIKE '%e'
-	);
+);
 
 SELECT hire_date
 FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+	AND (
+	birth_date LIKE '%-12-25'
+);
 
 SELECT birth_date
 FROM employees
