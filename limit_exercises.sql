7ee345294ce7c2eed27991d5cef25352464d43ef
Niraj <niraj.mulji@gmail.com>
2016-05-09 11:44:43 -0500

    This is my exercise after step 3.

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -3,3 +3,12 @@ FROM employees
 WHERE last_name LIKE 'Z%'
 GROUP BY last_name DESC
 LIMIT 10;
+
+SELECT first_name, last_name, hire_date, birth_date
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+	AND (
+	birth_date LIKE '%-12-25'
+)
+GROUP BY birth_date ASC, hire_date DESC
+LIMIT 5;
